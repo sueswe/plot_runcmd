7573665eefbdb5ff94371a00a9cd503823f447e6
Werner Süß <werner.suess@itsv.at>
2023-07-06 08:48:56 +0200

    data-dir anlegen

--- rohdaten.sh
+++ rohdaten.sh
@@ -6,6 +6,7 @@ if [[ -z $programm ]]; then
   exit 1
 fi
 
+mkdir data || echo "data-dir vorhanden"
 cd data || exit 2
 grep -i $programm runcmd_2023-*.csv |sed 's/ //g' > ${programm}.data || exit 3
 grep $programm ${programm}.data |  sed 's/t(s)://g' | awk -F';' '{print $2 " ; " $6  }' > x-y.csv || exit 4
