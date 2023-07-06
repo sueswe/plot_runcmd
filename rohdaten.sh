088158abc858dbc208ce916cc00ad6a1549482f0
Werner Süß <werner.suess@itsv.at>
2023-07-06 06:33:56 +0200

    REFACTOR: rohdaten.sh

--- rohdaten.sh
+++ rohdaten.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 programm=$1
 if [[ -z $programm ]]; then
   echo "Usage: $0 programmname"
@@ -8,8 +7,7 @@ if [[ -z $programm ]]; then
 fi
 
 cd data || exit 2
-grep -i $programm runcmd_2023-*.csv |sed 's/ //g' > ${programm}.data || exit 3
+grep -i $programm runcmd_2023-*.csv |sed 's/ //g' > ${programm}.csv || exit 3
 grep $programm ${programm}.data |  sed 's/t(s)://g' | awk -F';' '{print $2 " ; " $6  }' > x-y.csv || exit 4
-
 
 exit 0
